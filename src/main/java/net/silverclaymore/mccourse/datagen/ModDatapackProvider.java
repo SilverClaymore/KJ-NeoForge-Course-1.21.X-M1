0bd4337bbfa829b6dc73c4a761804451b8e956bc
SilverClaymore <68430916+SilverClaymore@users.noreply.github.com>
2024-10-02 23:27:21 +0200

    Moved the builder of the Custom Armor Trim inside ModWorldGenProvider. in this way BUILDER is not declared twice and it works.

--- src/main/java/net/silverclaymore/mccourse/datagen/ModDatapackProvider.java
+++ src/main/java/net/silverclaymore/mccourse/datagen/ModDatapackProvider.java
@@ -11,6 +11,9 @@ import net.silverclaymore.mccourse.trim.ModTrimPatterns;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+
+// This code overlaps with ModWorldGenProvider and it is not used otherwise BUILDER will be registered twice
+
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
--- src/main/java/net/silverclaymore/mccourse/datagen/ModWorldGenProvider.java
+++ src/main/java/net/silverclaymore/mccourse/datagen/ModWorldGenProvider.java
@@ -7,6 +7,8 @@ import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.silverclaymore.mccourse.MCCourseMod;
+import net.silverclaymore.mccourse.trim.ModTrimMaterials;
+import net.silverclaymore.mccourse.trim.ModTrimPatterns;
 import net.silverclaymore.mccourse.worldgen.ModBiomeModifiers;
 import net.silverclaymore.mccourse.worldgen.ModConfiguredFeatures;
 import net.silverclaymore.mccourse.worldgen.ModPlacedFeatures;
@@ -18,7 +20,10 @@ public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
             .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
-            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap)
+            .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap) // in the lecture this was inside ModDatapackProvider
+            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap) // in the lecture this was inside ModDatapackProvider
+            ;
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(MCCourseMod.MOD_ID));
