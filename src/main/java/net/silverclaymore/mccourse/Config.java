54590645ef93e24da7ead79fd7fed7feefbfa6d7
SilverClaymore <68430916+SilverClaymore@users.noreply.github.com>
2025-06-12 01:15:33 +0200

    updated neo_version to 21.1.177 changed the definition of ITEM_STRINGS because the previous one was deprecated

--- src/main/java/net/silverclaymore/mccourse/Config.java
+++ src/main/java/net/silverclaymore/mccourse/Config.java
@@ -32,9 +32,11 @@ public class Config
             .define("magicNumberIntroduction", "The magic number is... ");
 
     // a list of strings that are treated as resource locations for items
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
+    private static final ModConfigSpec.ConfigValue<List<String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+            .define("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+
+
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
